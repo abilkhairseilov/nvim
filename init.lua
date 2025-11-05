0aa2025c86fa5f546230afa223126f240a4091de
Abilkhair Seilov <abilkhairseilov@outlook.com>
2025-11-05 17:36:54 +0300

    [feat] init commit

---
+++ init.lua
@@ -0,0 +1,175 @@
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.signcolumn = "yes"
+-- vim.o.completeopt = "fuzzy"
+vim.o.winborder = "rounded"
+vim.o.swapfile = false
+vim.o.undofile = true
+vim.o.termguicolors = true
+vim.o.wrap = false
+vim.g.mapleader = " "
+
+vim.o.background = dark
+
+vim.wo.foldlevel = 99
+vim.wo.conceallevel = 2
+
+--- PACKAGING
+--- note: neovim v0.12 and up required (nightly as of time of implementation)
+vim.pack.add({
+	{ src = "https://github.com/nvim-mini/mini.pick" },
+	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/folke/snacks.nvim" },
+	{ src = "https://github.com/nvim-mini/mini.icons" },
+	{ src = "https://github.com/Saghen/blink.cmp" },
+	{ src = "https://github.com/norcalli/nvim-colorizer.lua" },
+	{ src = "https://github.com/folke/zen-mode.nvim" },
+	{ src = "https://github.com/folke/trouble.nvim",             { opt = true } },
+
+	-- lsp has caused me so much trouble lately
+	-- might as well as add this while im at adding plugins
+	{ src = "nvim-lspconfig",                                    { type = "git", opt = true } },
+
+	-- colorschemes
+	{ src = "https://github.com/catppuccin/nvim",                name = "catppuccin" },
+	{ src = "https://github.com/bjarneo/pixel.nvim" },
+
+	-- neorg and deps
+	{ src = "https://github.com/nvim-neorg/neorg" },
+	{ src = "https://github.com/nvim-neorg/lua-utils.nvim" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+	{ src = "https://github.com/pysan3/pathlib.nvim" },
+	{ src = "https://github.com/nvim-neotest/nvim-nio" },
+	{ src = "https://github.com/nvim-lua/plenary.nvim" },
+
+})
+
+--- PACKAGE CONFIG
+
+require("colorizer").setup {
+	hyprlang = { rgb_fn = true, },
+}
+
+require("oil").setup()
+require("mini.pick").setup()
+require("mini.icons").setup()
+require("neorg").setup({
+	load = {
+		["core.defaults"] = {},
+		["core.concealer"] = {},
+		["core.dirman"] = {
+			config = {
+				workspaces = {
+					notes = "~/notes",
+				},
+				default_workspace = "notes",
+			},
+		},
+		["core.export"] = {},
+		["core.presenter"] = {
+			config = {
+				zen_mode = "zen-mode",
+			}
+		},
+		["core.summary"] = {},
+	},
+}
+)
+require("zen-mode").setup()
+
+require("nvim-treesitter.configs").setup {
+	-- A list of parser names, or "all" (the listed parsers MUST always be installed)
+	ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "norg_meta", "norg" },
+
+	-- Install parsers synchronously (only applied to `ensure_installed`)
+	sync_install = false,
+
+	-- Automatically install missing parsers when entering buffer
+	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+	auto_install = true,
+
+	---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+	-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+	highlight = {
+		enable = true,
+
+		disable = function(lang, buf)
+			local max_filesize = 5 * 1024 * 1024 -- 5.0 MiB
+			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+			if ok and stats and stats.size > max_filesize then
+				return true
+			end
+		end,
+
+		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+		-- Using this option may slow down your editor, and you may see some duplicate highlights.
+		-- Instead of true it can also be a list of languages
+		additional_vim_regex_highlighting = false,
+	},
+}
+
+require("snacks").setup(
+	{
+		bufdelete = { enabled = true }
+	}
+)
+
+require("blink.cmp").setup()
+require("trouble").setup({})
+
+--- KEYBINDS
+vim.keymap.set("n", "<leader>s", ":update<CR>:so<CR>")
+vim.keymap.set("n", "<leader>w", ":write<CR>")
+vim.keymap.set("n", "<leader>q", ":quit<CR>")
+vim.keymap.set("n", "<leader>h", ":nohl<CR>")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<leader>[", ":bprev<CR>")
+vim.keymap.set("n", "<leader>]", ":bnext<CR>")
+vim.keymap.set("n", "<leader>d", ":bdel<CR>")
+
+vim.keymap.set("n", "<leader>e", ":Oil<CR>")
+vim.keymap.set("n", "<leader>f", ":Pick files<CR>")
+vim.keymap.set("n", "<leader>fb", ":Pick buffers<CR>")
+---vim.keymap.set("n", "<leader>h", ":Pick help<CR>")
+vim.keymap.set("n", "<leader><leader>", ":Pick grep_live<CR>")
+vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>lq", ":Trouble diagnostics<CR>")
+
+--- LSP
+local lspconfig = require("lspconfig")
+
+-- A general on_attach function to be used by ALL language servers
+local on_attach = function(client, bufnr)
+	-- Essential checks to prevent issues with ephemeral buffers (oil.nvim, diffs)
+	if not vim.api.nvim_buf_is_valid(bufnr) or vim.bo[bufnr].buftype ~= "" then
+		return
+	end
+
+	vim.schedule(function()
+		-- Set up buffer local keymaps when LSP attaches
+		-- You can customize these keymaps as you like
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover)
+		vim.keymap.set("n", "gri", vim.lsp.buf.implementation)
+		vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help)
+		vim.keymap.set("n", "grn", vim.lsp.buf.rename)
+		vim.keymap.set("n", "gra", vim.lsp.buf.code_action)
+	end)
+end
+
+lspconfig.lua_ls.setup({ on_attach = on_attach })
+lspconfig.clangd.setup({ on_attach = on_attach })
+lspconfig.basedpyright.setup({ on_attach = on_attach })
+lspconfig.bashls.setup({ on_attach = on_attach })
+lspconfig.ruff.setup({ on_attach = on_attach })
+
+vim.cmd("set completeopt=menu,preview,noselect")
+
+--- FINAL SETTINGS
+---
+vim.cmd("colorscheme quiet")

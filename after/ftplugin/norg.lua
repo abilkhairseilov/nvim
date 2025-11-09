fa79e806a4369f0d7825c064ce4accb54373470d
Abilkhair Seilov <abilkhairseilov@outlook.com>
2025-11-09 08:46:52 +0300

    [feat] add neorg keymaps

--- after/ftplugin/norg.lua
+++ after/ftplugin/norg.lua
@@ -1,3 +1,7 @@
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
+
+vim.keymap.set("n", "<leader>ni", ":Neorg inject-metadata<CR>")
+vim.keymap.set("n", "<leader>nw", ":Neorg workspace<CR>") 
+vim.keymap.set("n", "<leader>nt", ":Neorg toggle-concealer<CR>")

f5eccc3beb95057a82a5f6cc109631d888a77bbb
Abilkhair Seilov <abilkhairseilov@outlook.com>
2025-11-14 16:37:52 +0300

    update

--- after/ftplugin/norg.lua
+++ after/ftplugin/norg.lua
@@ -3,5 +3,13 @@ vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 
 vim.keymap.set("n", "<leader>ni", ":Neorg inject-metadata<CR>")
-vim.keymap.set("n", "<leader>nw", ":Neorg workspace<CR>") 
+vim.keymap.set("n", "<leader>nw", ":Neorg workspace<CR>")
 vim.keymap.set("n", "<leader>nt", ":Neorg toggle-concealer<CR>")
+
+function Surround(char)
+	vim.cmd("normal! i" .. char .. string.char(27) .. "`>a" .. char .. string.char(27))
+end
+
+vim.keymap.set("v", "<leader>nb", function()
+	Surround("*")
+end)

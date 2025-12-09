48ee612aa36841d48037d5e7477a31fee2ee0f19
Abilkhair Seilov <abilkhairseilov@outlook.com>
2025-12-09 16:40:04 +0300

    change norg ftplugin

--- after/ftplugin/norg.lua
+++ after/ftplugin/norg.lua
@@ -1,15 +1,24 @@
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
-vim.o.wrap = true
+vim.o.wrap = false
+vim.o.textwidth=80
+vim.o.formatoptions = "atw"
 
-vim.opt.linebreak = true
-vim.opt.breakindent = true
-vim.opt.breakindentopt = "shift:2"
+-- vim.opt.linebreak = true
+-- vim.opt.breakindent = true
+-- vim.opt.breakindentopt = "shift:2"
 
-vim.keymap.set("n", "<leader>ni", ":Neorg inject-metadata<CR>")
-vim.keymap.set("n", "<leader>nw", ":Neorg workspace<CR>")
-vim.keymap.set("n", "<leader>nt", ":Neorg toggle-concealer<CR>")
+local keymapping = vim.api.nvim_buf_set_keymap
+
+keymapping(0, "n", "<leader>ni", ":Neorg inject-metadata<CR>", {noremap = true})
+keymapping(0, "n", "<leader>nw", ":Neorg workspace<CR>", {noremap = true})
+keymapping(0, "n", "<leader>nt", ":Neorg toggle-concealer<CR>", {noremap = true})
+
+-- keymapping(0, "n", "j", "gj", {noremap = true})
+-- keymapping(0, "n", "k", "gk", {noremap = true})
+-- keymapping(0, "n", "0", "g0", {noremap = true})
+-- keymapping(0, "n", "$", "g$", {noremap = true})
 
 -- indenting
-vim.api.nvim_buf_set_keymap(0, "n", "<leader>lf", "gg=G<C-i>", {noremap = true})
+keymapping(0, "n", "<leader>lf", "gg=G<C-o>", {noremap = true})
